5d80e8ff0ea9c846db409491c496f00ab5b4d607
Ghost Frog <info@ghostfrog.co.uk>
2026-05-10 18:40:51 +0100

    Add production-only Google Analytics tag

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -46,6 +46,20 @@
             })();
         </script>
 
+        @if (app()->environment('production'))
+            <script async src="https://www.googletagmanager.com/gtag/js?id=G-XT0VG408ZJ"></script>
+            <script>
+                window.dataLayer = window.dataLayer || [];
+
+                function gtag() {
+                    dataLayer.push(arguments);
+                }
+
+                gtag('js', new Date());
+                gtag('config', 'G-XT0VG408ZJ');
+            </script>
+        @endif
+
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
 
